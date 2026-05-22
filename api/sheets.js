af149f1a01d8b33ca7853b333302e2f3fed763c7
reporting-boop <reporting@techno-communications.com>
2026-05-22 16:39:25 -0600

    Update sheets.js

--- api/sheets.js
+++ api/sheets.js
@@ -1,1 +1,13 @@
+// api/sheets.js
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxrnSRG9bewdk-BRaGZWIJwTyZjiiyLAUiJkSPdZSpQKQj7bX1Zh2LnPDasou558vfx7Q/exec";
 
+export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  try {
+    const response = await fetch(GAS_URL, { redirect: "follow" });
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+}
